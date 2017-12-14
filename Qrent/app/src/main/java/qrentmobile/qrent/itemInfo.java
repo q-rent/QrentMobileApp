21057e2157f6fe4b04c70023fb769ce8fb198dd9
kharya0 <kkdvillegas@slu.edu.ph>
2017-12-15 01:54:21 +0800

    Made new activity.

---
+++ Qrent/app/src/main/java/qrentmobile/qrent/itemInfo.java
@@ -0,0 +1,13 @@
+package qrentmobile.qrent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class itemInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_info);
+    }
+}
