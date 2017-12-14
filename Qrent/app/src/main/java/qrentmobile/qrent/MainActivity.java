4de026190ffc4339d064bb308699a711fe9395b1
David Paul Brackin <dev.mintgames@gmail.com>
2017-12-15 00:54:06 +0800

    added new stuff

--- Qrent/app/src/main/java/qrentmobile/qrent/MainActivity.java
+++ Qrent/app/src/main/java/qrentmobile/qrent/MainActivity.java
@@ -2,6 +2,8 @@ package qrentmobile.qrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView click = (ImageView)findViewById(R.id.imageGopro);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start new act
+            }
+        });
     }
 }
